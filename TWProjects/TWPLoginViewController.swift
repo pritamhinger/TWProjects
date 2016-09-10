be7fc8fe8df107d50d12db599047ed509ac52567
pritam hinger <pritamgenius24@gmail.com>
2016-09-10 20:09:26 +0530

    refactor: Add groups in xcode required to manage codebase

---
+++ TWProjects/TWPLoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TWProjects
+//
+//  Created by Pritam Hinger on 10/09/16.
+//  Copyright © 2016 AppDevelapp. All rights reserved.
+//
+
+import UIKit
+
+class TWPLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
